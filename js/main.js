b59d62a2e3df6ef22d1f18c685e473419d6ae888
Stefano <stefano.funaro@gmail.com>
2021-07-30 15:22:45 +0200

    array_of_objects_printed_onScreen

--- js/main.js
+++ js/main.js
@@ -16,7 +16,47 @@ for (let key in studente){
    console.log(student)
 }
 // Creare un array di oggetti di studenti.
+let students = [
 
+ {
+    name : 'alfredo',
+    lastname : 'fasullo',
+    eta : '25'
+},
+ {
+    name : 'alfredo1',
+    lastname : 'fasullo1',
+    eta : '25+1'
+},
+ {
+    name : 'alfredo2',
+    lastname : 'fasullo2',
+    eta : '25+2'
+},
+ {
+    name : 'alfredo3',
+    lastname : 'fasullo3',
+    eta : '25+3'
+},
+ {
+    name : 'alfredo4',
+    lastname : 'fasullo4',
+    eta : '25+4'
+},
+]
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-
+var studentName = document.getElementById('nome');
+var studentLastname = document.getElementById('cognome');
+var nameVariable;
+var lastnameVariable;
+for (i = 0; i<students.length; i++){
+    nameVariable = document.createTextNode(students[i]['name'])
+    studentName.appendChild(nameVariable);
+    studentName.appendChild(document.createElement('br'));
+    console.log(studentName)
+    lastnameVariable = document.createTextNode(students[i]['lastname'])
+    studentLastname.appendChild(lastnameVariable);
+    studentLastname.appendChild(document.createElement('br'));
+    console.log(studentLastname)
+}
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
